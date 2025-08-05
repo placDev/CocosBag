97c55a8775b0203568e729023d03329a9e29fd74
vyakubchik <vyache.yakubchik@x5.ru>
2025-08-05 23:54:35 +0300

    [~] Cocos Bag

---
+++ extensions/vue3-template/source/panels/default/index.ts
@@ -0,0 +1,69 @@
+/* eslint-disable vue/one-component-per-file */
+
+import { readFileSync } from 'fs-extra';
+import { join } from 'path';
+import { createApp, App, defineComponent } from 'vue';
+const panelDataMap = new WeakMap<any, App>();
+/**
+ * @zh 如果希望兼容 3.3 之前的版本可以使用下方的代码
+ * @en You can add the code below if you want compatibility with versions prior to 3.3
+ */
+// Editor.Panel.define = Editor.Panel.define || function(options: any) { return options }
+module.exports = Editor.Panel.define({
+    listeners: {
+        show() { console.log('show'); },
+        hide() { console.log('hide'); },
+    },
+    template: readFileSync(join(__dirname, '../../../static/template/default/index.html'), 'utf-8'),
+    style: readFileSync(join(__dirname, '../../../static/style/default/index.css'), 'utf-8'),
+    $: {
+        app: '#app',
+        text: '#text',
+    },
+    methods: {
+        hello() {
+            if (this.$.text) {
+                this.$.text.innerHTML = 'hello';
+                console.log('[cocos-panel-html.default]: hello');
+            }
+        },
+    },
+    ready() {
+        const sceneCamera = Editor.Message.request('scene', 'query-current-camera').then(x => {
+            console.log('scene-camera', x);
+        });
+        if (this.$.text) {
+            this.$.text.innerHTML = 'Hello Cocos. 312324';
+        }
+        if (this.$.app) {
+            const app = createApp({});
+            app.config.compilerOptions.isCustomElement = (tag) => tag.startsWith('ui-');
+           
+            app.component('MyCounter', defineComponent({
+                data() {
+                    return {
+                        counter: 0,
+                    };
+                }, 
+                methods: {
+                    addition() {
+                        this.counter += 1;
+                    },
+                    subtraction() {
+                        this.counter -= 1;
+                    },
+                },
+                template: readFileSync(join(__dirname, '../../../static/template/vue/counter.html'), 'utf-8'),
+            }));
+            app.mount(this.$.app);
+            panelDataMap.set(this, app);
+        }
+    },
+    beforeClose() { },
+    close() {
+        const app = panelDataMap.get(this);
+        if (app) {
+            app.unmount();
+        }
+    },
+});
